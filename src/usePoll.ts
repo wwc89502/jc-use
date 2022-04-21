54a7ece761651e637d714cf2e3b9963f28b03868
Wang Weichen <wwc89502@gmail.com>
2022-04-21 18:50:11 +0800

    Update usePoll.ts

--- src/usePoll.ts
+++ src/usePoll.ts
@@ -1,32 +1,29 @@
-import { debounce } from 'lodash';
-
 /**
  * @description 轮询
- * @usage poll.begin(pollHandle, pollTime)
+ * @usage poll.begin(pollHandle, wait)
  * @usage poll.stop()
  */
 export function usePoll() {
-  let debounced: any = null;
+  let pollTimer: any = null;
   return {
     /**
      * @description 开始轮询
      * @param pollHandle 需要轮询的函数
-     * @param pollTime 间隔时间
+     * @param wait 间隔时间
      */
     begin(pollHandle: any, wait: number = 1000) {
-      debounced = debounce(doPoll, wait);
-      async function doPoll() {
+      if (pollTimer) return false;
+      (async function doPoll() {
         await pollHandle?.();
-        debounced();
-      }
-      doPoll();
+        pollTimer = setTimeout(doPoll, wait);
+      })()
     },
     /**
      * @description 停止轮询
      */
     stop() {
-      debounced?.cancel();
-      debounced = null;
+      clearTimeout(pollTimer);
+      pollTimer = null;
     },
   };
 }
